5356a0700b9b7c8701e28083e9fbdedc6bd34ae0
Rudy <rudysw05@knights.ucf.edu>
2019-08-19 10:35:54 -0400

    initial commit

---
+++ data_prep.py
@@ -0,0 +1,176 @@
+"""
+This will probably become more important if/when cleaning the
+weather data.
+"""
+
+import pandas as pd 
+from eda import count_column_nans
+
+def read_training(index_columns=None, both=False, weather=False):
+    """
+    read the training X and y (optional) files
+    
+    Args:
+        index_cols: list - the column name(s) to index on
+
+        both: bool - whether or not to read in the target file too
+        weather: bool - if True the weather data file will be read and
+                 merged to the training data
+    Returns:
+        raw_X_train: pd.DataFrame - the data with only prepocessing being
+                     completed at this stage being what should be parsed as
+                     dates and what is the index column or the data merged with
+                     the weather data. If this is the case then X ffills the nan
+                     values to make the focus be on filling the observations without
+                     weather data.
+            or
+        raw_X_train, raw_y_train: tuple of pd.DataFrames - returned when both=True
+    """
+    if weather:
+        raw_X_train = pd.read_csv('data\\train_X.csv', parse_dates=['date'])
+        raw_weather = pd.read_csv('data\\weather_data.csv', parse_dates=['date'])
+
+        raw_X_train = ffill_nans(raw_X_train)
+        raw_X_train = raw_X_train.merge(raw_weather, how='left', on=['date','hour'])
+        raw_X_train = raw_X_train.set_index(index_columns)
+
+    else:
+        raw_X_train = pd.read_csv(
+            'data\\train_X.csv',
+             parse_dates=['date'],
+             index_col=index_columns)
+    if both:
+        raw_y_train = pd.read_csv(
+            'data\\train_y.csv',
+            parse_dates=['date'],
+            index_col=index_columns)
+
+        return raw_X_train, raw_y_train
+    
+    return raw_X_train
+
+
+def read_test_X(index_columns='warn', weather=False):
+    """
+    Read in the test data for the competition. There is no target vector y. The index
+    columns need to be the same as the training data.
+    """
+    if index_columns=='warn':
+        print('Make sure index_columns is set to same column(s) as in read_training.\
+            Reading in test_X.csv w/o any index column(s) specified.')
+        index_columns=None
+    
+    if weather:
+        raw_X_test = pd.read_csv('data\\test_X.csv', parse_dates=['date'])
+        raw_weather = pd.read_csv('data\\weather_data.csv', parse_dates=['date'])
+
+        raw_X_test = ffill_nans(raw_X_test)
+        raw_X_test = raw_X_test.merge(raw_weather, how='left', on=['date','hour'])
+        X_test = raw_X_test.set_index(index_columns)
+    
+    else:
+        X_test = pd.read_csv('data\\test_X.csv', parse_dates=['date'], index_col=index_columns)    
+    
+    return X_test
+
+
+#if wanting to deal with the data together
+def create_Xy_df(X_df, y_df, on_cols):
+    """
+    I like doing the data cleaning process with the data as
+    a whole. That way if any rows are dropped (which won't happen
+    in this competition) the target rows are also dropped. Perhaps
+    it's better to leave them split though and just pass the index
+    of the X to filter the y. In this way I won't risk accidentally
+    transforming the values in the target.
+    """
+    return pd.merge(X_df, y_df, how='inner', on=on_cols)
+
+def ffill_nans(df):
+    """
+    Data is expected to be in order by date and hour (otherwise
+    use sort on those two columns before hand). This replaces the
+    NaN with the value, of the same feature, from the preceeding 
+    observation.
+    """
+    return df.fillna(method='ffill', axis=0)
+
+
+def fill_weather_forecast_columns(df):
+    """
+        ffill, bfill, ffill should fill them appropriately. First, Jan. 1st needs all hours
+        data. To do this we will use Jan. 2nd numbers. This doesn't seem to produce a smooth
+        fix as the number for wind_north_OK goes from 2.11 to -2.44 but it's a start.
+
+        I will set limits on the ffill/bfill so that it only fills the NaN of the hour that
+        it 'touches'.
+
+        Args:
+            df: pd.DataFrame - the dataframe that needs filling
+        
+        Returns:
+            filled_df: pd.DataFrame - the dataframe that has the nan's filled
+    """
+
+    filled_df = df.copy()
+    filled_df.loc['2018-01-01','temp_KC':'wind_north_SD'] = filled_df.loc['2018-01-02','temp_KC':'wind_north_SD'].values
+    filled_df.loc['2018-02-06','temp_KC':'wind_north_SD'] = filled_df.loc['2018-02-05','temp_KC':'wind_north_SD'].values
+    filled_df.loc['2019-02-05','temp_KC':'wind_north_SD'] = filled_df.loc['2019-02-04','temp_KC':'wind_north_SD'].values
+    # print(filled_df.isna().sum(axis=0))
+    filled_df = filled_df.fillna(method='ffill', limit=1)
+    # print(filled_df.isna().sum(axis=0))
+    filled_df = filled_df.fillna(method='bfill', limit=1)
+    # print(filled_df.isna().sum(axis=0))
+    filled_df = filled_df.fillna(method='ffill', limit=1)
+
+    any_nans = filled_df.isna().sum(axis=0)
+    
+    if any_nans.sum(axis=0) != 0:
+        print('The function did not convert all NaNs. Some NaNs still exist.')
+
+    return filled_df
+
+
+        
+if __name__ == '__main__':
+
+    pass
+    #w/ weather data
+    #----------------
+    # XW_train, y_train = read_training(index_columns=['date'], both=True, weather=True)
+
+    # XW_train = fill_weather_forecast_columns(XW_train)
+    # print(XW_train.info())
+    # print(y_train.info())
+    # XW_train.to_csv('data\\XW_train.csv')
+
+    # X = XW_train.copy()
+    # print(X.loc['2018-01-01':'2018-01-02', 'temp_KC':'wind_north_SD']) #2018-01-01 needs all the data
+    # X.loc['2018-01-01', 'temp_KC':'wind_north_SD'] = X.loc['2018-01-02', 'temp_KC':'wind_north_SD'].values
+    # print(X.loc['2018-01-01':'2018-01-02', 'coal':'wind_north_SD'])
+    # example = XW_train.loc['2018-07-10':'2018-07-11',['hour','wind_north_OK']] 
+    # print(example.isna().sum(axis=0))
+    # print(example.isna().sum(axis=0).sum(axis=0))
+    # example = example.fillna(method='ffill', axis=0, limit=1)
+    # print(example)
+    # example = example.fillna(method='bfill', axis=0, limit=1)
+    # print(example)
+    # example = example.fillna(method='ffill', axis=0, limit=1)
+    # print(example)
+    #running the model still works as expected when no weather data
+    
+
+
+    #w/o weather data
+    #-----------------
+    # X_train = read_training(index_columns=['date'], both=False)
+    # #fill na by ffill
+    # print(count_column_nans(X_train))
+    # X_train_ffilled = ffill_nans(X_train)
+    # print(count_column_nans(X_train_ffilled))
+
+    # #send to csv
+    # X_train_ffilled.to_csv('data\\ffilled_X_train.csv')
+
+    # X_test = read_test_X(index_columns=['date'])
+    # print(X_test.head())
---
+++ models.py
@@ -0,0 +1,252 @@
+from math import sqrt
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.metrics import mean_squared_error, make_scorer
+from sklearn.linear_model import LinearRegression, Lasso 
+from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
+import data_prep as dp
+
+
+#first model will run linear regression
+def train_linear_regression(X_data, y_data, split=False, train_pct=None, rand_state=None):
+    """
+    Fit a linear regression model and return the model. If split=True,
+    return the model and the validation MSE. Note, this model ignores the
+    time series aspect of the data.
+    
+    Args:
+        X_data: pd.DataFrame - the feature matrix
+        y_data: array (n_samples, n_targets)- the target array
+        split: bool (default: False) - Train and validate if True or just 
+               train if False
+        train_pct: float btwn 0 & 1 - signifies the train test split size
+        rand_state: int (default: None) - to create reproducible results
+    
+        Returns:
+            (linreg, y_pred)
+            linreg: sklearn LinearRegression model - the model resulting from the fit on
+                    either the full dataset (split=False) or fit on the training data
+                    within the function (split=True)
+    
+                or
+            (linreg, mse, rmse) tuple if split=True
+            mse: float or array of shape (n_targets,) - the mse calculated if split=True
+            rmse: float or array of shape (n_targets,) - the root mse calculated if split=True
+
+    """
+    if split:
+        X_train, X_test, y_train, y_test = train_test_split(
+            X_data, 
+            y_data, 
+            random_state=rand_state,
+            train_size=train_pct
+            )
+
+        linreg = LinearRegression(n_jobs=-1)
+        linreg.fit(X_train, y_train)
+        y_pred = linreg.predict(X_test)
+
+        #compute MSE using mean_squared_error
+        #by default multi_output='uniform_average' which is why it was returning a single value
+        mse = mean_squared_error(y_test, y_pred, multioutput='raw_values')
+        rmse = np.sqrt(mse)
+
+        return linreg, mse, rmse
+
+    else:
+        linreg = LinearRegression(n_jobs=-1)
+        linreg.fit(X_data, y_data)
+      
+        return linreg
+
+    
+def train_lasso_regression(X_data, y_data, alpha=1, split=False, cross_val=False, folds=5, train_pct=None, rand_state=None):
+    """
+    """
+    if split and cross_val:
+        raise ValueError('Args split and cv cannot both be set to True.')
+
+    elif split: #single train and test run
+        X_train, X_test, y_train, y_test = train_test_split(
+            X_data,
+            y_data, 
+            random_state=rand_state,
+            train_size=train_pct
+            )
+
+        lasso = Lasso(random_state=rand_state, alpha=alpha, tol=0.01)
+        lasso.fit(X_train, y_train)
+        y_pred = lasso.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred, multioutput='raw_values')
+        rmse = np.sqrt(mse)
+
+        return lasso, mse, rmse
+    
+    elif cross_val:
+        lasso = Lasso(random_state=rand_state, alpha=alpha)
+        mse_scorer = make_scorer(mean_squared_error, multioutput='uniform_average', greater_is_better=False)
+        #a scalar must be returned, not an array
+        cv_scores = cross_validate(lasso, X_data, y_data, cv=folds, scoring=mse_scorer)
+        return cv_scores
+        
+    else:
+        pass
+
+
+def train_random_forest_regression(X_data, y_data, split=False, train_pct=None, rand_state=None,
+                                 ntrees=1, min_samples_node=1):
+    """
+    Fit a linear regression model and return the model. If split=True,
+    return the model and the validation MSE. Note, this model ignores the
+    time series aspect of the data.
+    
+    Args:
+        X_data: pd.DataFrame - the feature matrix
+        y_data: array (n_samples, n_targets)- the target array
+        split: bool (default: False) - Train and validate if True or just 
+               train if False
+        train_pct: float btwn 0 & 1 - signifies the train test split size
+        rand_state: int (default: None) - to create reproducible results
+    
+        Returns:
+    """
+    if split:
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            X_data,
+            y_data, 
+            random_state=rand_state,
+            train_size=train_pct
+            )
+
+        #above should produce the same sets given the same rand_state is given
+        rfr = RandomForestRegressor(
+            n_estimators=ntrees,
+            max_features='sqrt',
+            min_samples_leaf=min_samples_node,
+            random_state=rand_state,
+            oob_score=False
+        )
+
+        rfr.fit(X_train, y_train)
+        y_pred = rfr.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred, multioutput='raw_values')
+        rmse = np.sqrt(mse)
+
+        return rfr, mse, rmse
+
+    #use out-of-bag error
+    else:    
+    
+        rfr = RandomForestRegressor(n_estimators=ntrees, max_features='sqrt',
+                                    min_samples_leaf=min_samples_node,
+                                    random_state=rand_state, oob_score=True)
+        rfr.fit(X_data, y_data)
+        y_oob_pred = rfr.oob_prediction_
+        oob_mse = ((y_data - y_oob_pred)**2).mean() #should maintain order
+        oob_rmse = sqrt(oob_mse)
+
+        return rfr, oob_mse, oob_rmse
+
+
+
+if __name__ == '__main__':
+
+
+    ## perform Linear Regression without the weather data
+    #=========================================================
+    # X, y = dp.read_training(index_columns=['date'], both=True)
+
+    # X_feed = dp.ffill_nans(X)
+    # y_feed = y.loc[:,['target1','target2']].values
+    
+    # model, mse, rmse = train_linear_regression(
+    #     X_feed,
+    #     y_feed,
+    #     split=True,
+    #     train_pct=0.8, 
+    #     rand_state=5
+    #     )
+    
+    # print(f'Linear Regression RMSE: {rmse}')
+
+    ### submit linear regression model
+    #----------------------------------------
+    # linear_regression = train_linear_regression(X_feed, y_feed, split=False)
+    # X_test = dp.read_test_X(index_columns=['date'])
+    # linear_regression.fit(X_feed, y_feed)
+    # turn_in_pred = linear_regression.predict(dp.ffill_nans(X_test))
+
+    # from submit_model import upload_response
+    # upload_response('RW-LinearRegression', turn_in_pred)
+
+    ## perform Linear Regression with the weather data
+    X_weather, y_weather = dp.read_training(
+        both=True, 
+        weather=True, 
+        index_columns=['date']
+        )
+
+    X_weather = dp.fill_weather_forecast_columns(X_weather)
+    y_weather = y_weather.loc[:,['target1','target2']].values
+    
+    lr_modelW, mseW, rmseW = train_linear_regression(
+        X_weather,
+        y_weather,
+        split=True,
+        train_pct=0.8,
+        rand_state=5
+        )
+
+    print(f'Linear Regression w/ Weather Data RMSE: {rmseW}')    
+    
+    # perform Random Forest Regression w/ Weather Data
+    # rfr_results = train_random_forest_regression(
+    #     X_data=X_weather,
+    #     y_data=y_weather,
+    #     ntrees=800,
+    #     split=True,
+    #     train_pct=0.8,
+    #     rand_state=5
+    # )
+
+    # print(f'Random Forest Regressor w/ Weather Data RMSE: {rfr_results[2]}')
+
+    ### submit random forest regressor model
+    #----------------------------------------
+    X_test = dp.read_test_X(index_columns=['date'], weather=True)
+    print(X_test.loc['2018-01-15':'2018-01-16', :])
+    X_test = X_test.fillna(method='ffill', limit=1)
+    print(X_test.loc['2018-01-15':'2018-01-16',:])
+    X_test = X_test.fillna(method='bfill',limit=1)
+    print(X_test.loc[X_test.isna().any(axis=1),:])
+    print(X_test.loc['2018-02-21':'2018-02-22'])
+
+    # from submit_model import upload_response
+    # upload_response('RW-LinearRegression_with_weather', turn_in_pred)
+    ## perform Lasso Regression with the weather data
+    # split_results = train_lasso_regression(
+    #     X_weather,
+    #     y_weather,
+    #     alpha=0.01,
+    #     split=True,
+    #     train_pct=0.8,
+    #     rand_state=5
+    #     )
+
+    ##gets a does not converge warning even after playing with max_iter and tol a bit
+    # print(f'Lasso w/ Weather Data (alpha=XXXXX) RMSE: {split_results[2]}')
+
+    ## perform Lasso regression without weather data
+    #===============================================
+    # split_results = train_lasso_regression(
+    #     X_feed,
+    #     y_feed,
+    #     alpha=0.1,
+    #     split=True,
+    #     train_pct=0.8,
+    #     rand_state=5
+    #     )
+    
+    # print(f'Lasso RMSE (alpha=0.1): {split_results[2]}')
